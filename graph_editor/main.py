f954131dbd89d32c8e7b0c446c471ceeb8a953e8
вадим сивцов <vsbim2007@gmail.com>
2026-09-08 01:01:51 +0700

    Добавлены графический редактор и README

---
+++ graph_editor/main.py
@@ -0,0 +1,43 @@
+﻿class Figure:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_coords(self):
+        return self._x, self._y
+
+    def set_coords(self, x, y):
+        self._x = x
+        self._y = y
+
+    def calculate_area(self):
+        return 0
+
+class Circle(Figure):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def calculate_area(self):
+        return 3.14159 * self.radius ** 2
+
+class Square(Figure):
+    def __init__(self, x, y, side):
+        super().__init__(x, y)
+        self.side = side
+
+    def calculate_area(self):
+        return self.side ** 2
+
+if __name__ == "__main__":
+    figures = [
+        Circle(0, 0, 5),
+        Square(10, 10, 4),
+        Circle(1, 2, 3),
+        Square(5, 5, 6),
+        Circle(-1, -1, 2)
+    ]
+    total_area = 0
+    for fig in figures:
+        total_area += fig.calculate_area()
+    print("Общая площадь всех фигур:", total_area)
